35ee498a5c59714f1e075ef349e68c3f17d885b2
kauan <kauan.celino17@gmail.com>
2024-06-25 15:45:26 -0300

    inicio entrar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,11 @@ const routes: Routes = [
   {
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },  {
+    path: 'entrar',
+    loadChildren: () => import('./entrar/entrar.module').then( m => m.EntrarPageModule)
   },
+
 ];
 
 @NgModule({
